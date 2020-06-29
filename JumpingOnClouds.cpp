ad08d749190da12ebb450824f0bd475bef619ff4
Alassane-DIAllo <44902470+Alassane-DIAllo@users.noreply.github.com>
2020-06-29 17:14:33 +0200

    Update JumpingOnClouds.cpp

--- JumpingOnClouds.cpp
+++ JumpingOnClouds.cpp
@@ -1,1 +1,13 @@
+#include <isotream>
+#include <vector>
+using namespace std;
 
+int jumpingOnClouds(vector<int> c) {
+    int Njump = 0;
+    for(int i =0; i<c.size()-1;i++,Njump++){
+        if(c[i+2]!=1 and i+2<c.size()) {
+            i++;
+        }
+    }
+ return Njump;
+};
