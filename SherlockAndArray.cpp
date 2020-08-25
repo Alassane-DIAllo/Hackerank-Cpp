7dcf88babba57800f3c2972631204440274bfcac
Alassane-DIAllo <44902470+Alassane-DIAllo@users.noreply.github.com>
2020-08-25 03:03:26 +0200

    Update SherlockAndArray.cpp

--- SherlockAndArray.cpp
+++ SherlockAndArray.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespaces std;
 
+int T ;
+cin>>T;
+int pre[];
+
+int main(){
+    while(T--){
+      int n;
+      cin>>n;
+      for(int i=0;i<n;i++){
+          int x;
+          cin>>x;
+          if(i==0) pre[i]=x;
+          else pre[i] =pre[i-1]+x;
+      }
+      bool flag = false;
+      for(int i=1; i<n;i++){
+         if (p[i-1] == p[n-1]-p[i]){
+            flag = true;
+            break;
+         }
+      }
+      if(flag==true) cout<<"YES\n";
+      else cout<<"NO\n";
+    }
+}
