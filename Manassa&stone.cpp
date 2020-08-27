7e65a9d8d9094d4e4549c83f145b6eae9a54b358
Alassane-DIAllo <44902470+Alassane-DIAllo@users.noreply.github.com>
2020-08-28 01:38:19 +0200

    Update Manassa&stone.cpp

--- Manassa&stone.cpp
+++ Manassa&stone.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+#include<set>
+#include<algorithm>
+#include<vector>
 
+
+vector<int> stones(int n, int a, int b) {
+   
+    set<int> s;
+    for(int i=n-1;i>=0;i--){
+        s.insert(a*i+b*(n-i-1));
+    }
+    vector<int> v;
+    for(set<int> ::iterator it =s.begin();it!=s.end();it++){
+        v.push_back(*it);
+    }
+    return v;
+}
